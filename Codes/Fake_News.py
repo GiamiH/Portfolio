956171e4d6f815ccf0183a33518a3cd2b0e94d84
Giami <huynhgiami@gmail.com>
2025-02-01 22:45:27 +0100

    open and read file

--- Codes/Fake_News.py
+++ Codes/Fake_News.py
@@ -3,8 +3,9 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-# Get the absolute path of the current script
+# Open and read file
 script_dir = Path(__file__).resolve().parent
 data_path = script_dir.parent / "Data" / "News.csv"
 data = pd.read_csv(data_path, index_col=0)
-print(data.head())
+#print(data.head())
+
