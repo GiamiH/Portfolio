ebf097c3aae81b77e5ff16ae06b1a95853386082
Giami <huynhgiami@gmail.com>
2025-02-02 13:25:00 +0100

    general overview

--- Codes/Fake_News.py
+++ Codes/Fake_News.py
@@ -8,4 +8,16 @@ script_dir = Path(__file__).resolve().parent
 data_path = script_dir.parent / "Data" / "News.csv"
 data = pd.read_csv(data_path, index_col=0)
 #print(data.head())
+#print(data.shape) #44,919 rows & 5 columns
+data = data.drop(["title", "subject","date"], axis = 1)
 
+# Shuffle data model to prevent bias
+data = data.sample(frac=1)
+data.reset_index(inplace=True)
+data.drop(["index"], axis=1, inplace=True)
+
+sns.countplot(data=data,
+              x='class',
+              order=data['class'].value_counts().index)
+plt.show()
+## change bar colors
